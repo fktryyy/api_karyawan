135746389d116d7e13dc9073683a3ae0f9c43c68
benny48 <arvan.benny@gmail.com>
2025-03-13 08:45:18 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\KaryawanController;
 use App\Http\Controllers\BankController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('karyawan', KaryawanController::class);
-Route::apiResource('bank', BankController::class);
+// Route::apiResource('karyawan', KaryawanController::class);
+// Route::apiResource('bank', BankController::class);
 Route::post('karyawan', [KaryawanController::class, 'store']);
